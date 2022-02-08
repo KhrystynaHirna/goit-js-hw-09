a5df8fa7548ce98bafa98b73bb726ef3d1211a02
KhrystynaHirna <86721411+KhrystynaHirna@users.noreply.github.com>
2022-02-08 20:27:05 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,35 +1,56 @@
 
 const btnStart = document.querySelector('[data-start]');
 const btnStop = document.querySelector('[data-stop]');
-const bodyColor = document.querySelector('body');
+
 const PROMPT_DELAY = 1000;
+const btnDisable = false;
 let timerId = null;
-
-// let promptCounter = 0;
-
 
 btnStart.addEventListener('click', onBtnStartClick);
 btnStart.addEventListener('click', onBtnStopClick);
-btnStart.disabled = false;
-btnStop.disabled = true;
 
-function onBtnStartClick() {
-    bodyColor.style.backgroundColor = getRandomHexColor();
-    console.log(bodyColor);
-    btnStop.disabled;
-    timerId = setInterval(() => {
-        onBtnStartClick();
-        // promptCounter += 1;
-    }, PROMPT_DELAY);
-    };
-
-
-function onBtnStopClick() {
-    // if (btnStart.disabled) {
-    // clearInterval(timerId);
-    // console.log(`Interval with id ${timerId} has stopped!`);
-    // }  
-}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+function onBtnStartClick() {
+    if (btnStart.disabled === btnDisable) {
+        btnStart.disabled = true;
+}
+    timerId = setInterval(() => {
+        document.body.style.background = getRandomHexColor();
+    }, PROMPT_DELAY);
+}
+function onBtnStopClick() {
+    clearInterval(timerId);
+    btnStart.disabled = btnDisable;
+}
+
+
+// const start = document.querySelector('[data-start]');
+// const stop = document.querySelector('[data-stop]');
+// // console.log(start);
+// // console.log(stop);
+// let timerId = null;
+// const btnDisable = false;
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// start.addEventListener('click', changeBgColor);
+
+// stop.addEventListener('click', stopChangingBgColor);
+
+// function changeBgColor() {
+//   if (start.disabled === btnDisable) {
+//     start.disabled = true;
+//   }
+//   timerId = setInterval(() => {
+//     document.body.style.background = getRandomHexColor();
+//   }, 1000);
+// }
+
+// function stopChangingBgColor() {
+//   clearInterval(timerId);
+//   start.disabled = btnDisable;
+// }
