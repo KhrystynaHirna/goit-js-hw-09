d3b37c4f20519c0cddab39e8a28f6e601e20cd44
KhrystynaHirna <86721411+KhrystynaHirna@users.noreply.github.com>
2022-02-09 05:41:21 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,29 @@
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// delayInput = document.querySelector('delay');
+// stepInput = document.querySelector('step');
+// amountInput = document.querySelector('amount');
+// button = document.querySelector('.form button');
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', onFormInput);
+
+function onFormInput(evt) {
+    formData[evt.target.name] = evt.target.value;
+    console.log(formData);
+};
+
+
+function onFormInput() {
+
+};
+
+function onFormSubmit() {
+
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +32,14 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
+
+
